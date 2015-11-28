9baefb74a14fa4e6df6123ce8c4469b98e04dfcd
Balamurali Ananthan <balamurali.ananthan@gmail.com>
2015-11-28 15:59:55 -0800

    Adding comments to the steps

--- mergesort/MergeSort.java
+++ mergesort/MergeSort.java
@@ -4,9 +4,16 @@ public class MergeSort
 {
     public static void main(String args[])
     {
+        // Array to be sorted
         int array[] = {52,33,11,62,48,97,22,31,39};
+        
+        // To show the unsorted array
         System.out.println(Arrays.toString(array));
+        
+        // Merge sort
         mergeSort(array, 0, array.length - 1); 
+        
+        // Sorted array 
         System.out.println(Arrays.toString(array));
     }
 
@@ -15,8 +22,14 @@ public class MergeSort
         if (i < r)
         {
             int j = (i + r) / 2;
+
+            // Divide the first half
             mergeSort(array, i, j);
+
+            // Divide the second half
             mergeSort(array, j+1, r);
+
+            // Merge them all
             merge(array, i, j, r);
         }
     }
@@ -27,6 +40,9 @@ public class MergeSort
         int tempPtr = 0;
         int i = left;
         int k = mid+1;
+
+        // Regular merge. Compare the values from each array and add it 
+        // to the temp array. 
         while(i <= mid && k <= right)
         {
            if(array[i] > array[k])
@@ -39,16 +55,19 @@ public class MergeSort
            }
         }
 
+        // Add the rest of the elements from first array if there are any
         while(i <= mid)
         {
             temp[tempPtr++] = array[i++];
         }
 
+        // Add the rest of the elements from second array if there are any
         while(k <= right)
         {
             temp[tempPtr++] = array[k++];
         }
 
+        // Copy them back to the original array
         tempPtr = 0;
         while(tempPtr < (right - left + 1))
         {
